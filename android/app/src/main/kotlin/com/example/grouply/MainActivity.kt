1aef25b507201422fd6cf4ccb90203272b46da58
Sami Cheema <102137871+SamiCh246@users.noreply.github.com>
2023-07-23 21:19:26 -0400

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/grouply/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.grouply
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:149954977588:web:d0230b7dc43685a4c94f61";
+  static String apiKey = "AIzaSyDn9PiIhtx8OEwxLJp3svbGVeOF1lF9y-s";
+  static String messagingSenderId = "149954977588";
+  static String projectId = "grouply-fff1f";
+  final primaryColor = Color.fromARGB(255, 23, 71, 169);
+}
